f5fd5955588fac5fb34610ca54708b06d4f335d8
Julio Freitas <julio.clf31@gmail.com>
2019-08-09 04:02:44 -0300

    Updates

---
+++ precache-manifest.a2e79667a02e9dce20b006af63f9a070.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "174f9b3f48003e6e92ba0119be8432c9",
+    "url": "/breaking-gh-api/index.html"
+  },
+  {
+    "revision": "33bbd9ae439c84050a5e",
+    "url": "/breaking-gh-api/static/css/2.5d45f07d.chunk.css"
+  },
+  {
+    "revision": "21e52fbf139d39cc6962",
+    "url": "/breaking-gh-api/static/css/main.571222d6.chunk.css"
+  },
+  {
+    "revision": "33bbd9ae439c84050a5e",
+    "url": "/breaking-gh-api/static/js/2.e01b2636.chunk.js"
+  },
+  {
+    "revision": "21e52fbf139d39cc6962",
+    "url": "/breaking-gh-api/static/js/main.883a9f59.chunk.js"
+  },
+  {
+    "revision": "4d925caa978cf6c3daaf",
+    "url": "/breaking-gh-api/static/js/runtime~main.750302ac.js"
+  },
+  {
+    "revision": "82875518c56277504efaea6f38c7ba44",
+    "url": "/breaking-gh-api/static/media/breaking-bad-logo.82875518.png"
+  }
+]);
